5eb4bd3bcb7a4e0adc7ac212683ed25a26e882f5
gaston <gastonsbodio@gmail.com>
2023-05-09 15:52:30 -0500

    improvments launch menu commands, maya 2023

--- launch_menu_commands.py
+++ launch_menu_commands.py
@@ -7,36 +7,38 @@ if dll.SHGetSpecialFolderPathW(None, buf, 0x0005, False):
     USER_DOC = buf.value
 SCRIPT_FOL = USER_DOC + "\\prod_manager\\jira_manager"
 sys.path.append( SCRIPT_FOL )
-import pymel.core as pm
 import helper as hlp
 import jira_project_manager as jiraM
+import definitions as de
 try:
     import importlib
     importlib.reload(hlp)
     importlib.reload( jiraM )
+    importlib.reload( de )
 except Exception:
     reload(hlp)
     reload(jiraM)
-    
+    reload(de)
+sys.path.append( de.PY_PACKAGES )
+import pymel.core as pm
 def launch_manager():
     widget = jiraM.MyMainWindow()
     widget.ui.show()
-    
 def update_tools():
     file_content = hlp.write_down_tools ( )
     hlp.create_python_file ('update_tools', file_content)
     hlp.run_py_stand_alone( 'update_tools', True)
-
 main_window = pm.language.melGlobals['gMainWindow']
 menu_obj='ManagerMenu'
 menu_lable='Jira Manager Tool'
-
-if pm.menu( menu_obj, label = menu_lable, exists = True, parent = main_window ):
-    pm.deleteUI( pm.menu( menu_obj, e=True,deleteAllItems=True ))
-    
+try:
+    if pm.menu( menu_obj, label = menu_lable, exists = True, parent = main_window ):
+        pm.deleteUI( pm.menu( menu_obj, e=True,deleteAllItems=True ))
+except Exception:
+    pass
 custom_tools_menu = pm.menu ( menu_obj, label = menu_lable, parent = main_window, tearOff=True)
 pm.menuItem( label='launch manager tool', command='launch_manager()' )
-#pm.menuItem( label='Rigging', subMenu=True, parent = custom_tools_menu , tearOff = True )
-#pm.menuItem( label='Mirror Joints', command='launch_manager()' )
+#pm.menuItem( label='example', subMenu=True, parent = custom_tools_menu , tearOff = True )
+#pm.menuItem( label='sub example', command='ecample_func()' )
 pm.setParent( '..', menu = True )
 pm.menuItem( label='update tools', command='update_tools()' )
